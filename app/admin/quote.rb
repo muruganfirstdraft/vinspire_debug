cd9221a4db228b74362cec97fcaa350068d3de25
Raghu Betina <raghu@firstdraft.com>
2017-01-02 10:04:20 +0000

    Whitelist columns in Quote for ActiveAdmin

--- app/admin/quote.rb
+++ app/admin/quote.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Quote do
 
+ permit_params :body, :author, :mood_id, :quote_type
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
